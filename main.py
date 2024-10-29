4a92d5c94973ed311b5192e905fe03a976e5c496
nazikhoma <128983249+nazikhoma@users.noreply.github.com>
2024-10-29 11:46:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 from datetime import datetime
 
 
-bot  = telebot.TeleBot("") #тут в лапки вставити токен з BotFather
+bot  = telebot.TeleBot("7735643250:AAF5ugGCCPjVwxpqmXuZZo2XdUs8jq5B6sk") #тут в лапки вставити токен з BotFather
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Привіт!")
